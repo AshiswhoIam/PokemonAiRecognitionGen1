4e6325329c6abb542ceabcb85d13070bb9213405
Ash <ashlegitimate@outlook.com>
2025-02-20 21:35:40 -0500

    Checking for gifs

--- ImageChecker.py
+++ ImageChecker.py
@@ -4,7 +4,8 @@ data_path = "PokemonDataGen1"
 
 #Array storage for folders that need fixing
 missing_img_folders = []
-
+#Storage for gifs to check which folders
+gif_folders = []
 # Loop through all folders
 #os.listdir(dataset_path) Gets list of all folder names using pokemon as var
 for pokemon in os.listdir(data_path):
@@ -20,7 +21,13 @@ for pokemon in os.listdir(data_path):
         if image_count != 100:
             missing_img_folders.append((pokemon, image_count))
 
-        print(f"{pokemon}: {image_count} images all good check check" if image_count == 100 else f"{pokemon}: {image_count} images not to folder XXXXXX")
+        #print(f"{pokemon}: {image_count} images all good check check" if image_count == 100 else f"{pokemon}: {image_count} images not to folder XXXXXX")
+
+        # Check for GIF files specifically
+        gif_files = [f for f in os.listdir(pokemon_path) if f.lower().endswith('.gif')]
+        # Append folders containing GIFs
+        if gif_files:
+            gif_folders.append((pokemon, len(gif_files)))
 
 
 if missing_img_folders:
@@ -29,3 +36,11 @@ if missing_img_folders:
         print(f"- {folder}: {count} images")
 else:
     print("\nAll the folders have 100imgs")
+
+
+if gif_folders != []:
+    print("\nThe following folders have gifs:")
+    for folder, count in gif_folders:
+        print(f"- {folder}: {count} gifs")
+else:
+    print("\nThe data has no gifs.")
