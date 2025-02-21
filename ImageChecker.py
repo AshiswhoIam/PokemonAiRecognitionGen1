e4d531c757c9108daa06a9eb27904734856e6f94
Ash <ashlegitimate@outlook.com>
2025-02-20 21:59:01 -0500

    Image type count

--- ImageChecker.py
+++ ImageChecker.py
@@ -6,6 +6,12 @@ data_path = "PokemonDataGen1"
 missing_img_folders = []
 #Storage for gifs to check which folders
 gif_folders = []
+
+#Variables to count each type of files
+jpg_count = 0
+jpeg_count = 0
+png_count = 0
+
 # Loop through all folders
 #os.listdir(dataset_path) Gets list of all folder names using pokemon as var
 for pokemon in os.listdir(data_path):
@@ -29,6 +35,11 @@ for pokemon in os.listdir(data_path):
         if gif_files:
             gif_folders.append((pokemon, len(gif_files)))
 
+        #JPG, JPEG, and PNG images
+        jpg_count += len([f for f in os.listdir(pokemon_path) if f.lower().endswith('.jpg')])
+        jpeg_count += len([f for f in os.listdir(pokemon_path) if f.lower().endswith('.jpeg')])
+        png_count += len([f for f in os.listdir(pokemon_path) if f.lower().endswith('.png')])
+
 
 if missing_img_folders:
     print("\nThe following pokemon folders don't have 100 images:")
@@ -44,3 +55,10 @@ if gif_folders != []:
         print(f"- {folder}: {count} gifs")
 else:
     print("\nThe data has no gifs.")
+
+#Total images and each file type
+print("\nTotal images types and total dataset:")
+print(f"- JPG images: {jpg_count}")
+print(f"- JPEG images: {jpeg_count}")
+print(f"- PNG images: {png_count}")
+print(f"- Total images: {jpg_count+jpeg_count+png_count}")
