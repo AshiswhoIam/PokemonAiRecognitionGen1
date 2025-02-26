fc1d101434247cde13cc3420bb5beb0f2e88530f
Ash <ashlegitimate@outlook.com>
2025-02-25 20:38:52 -0500

    Model Training for now

--- ModelTraining.py
+++ ModelTraining.py
@@ -44,4 +44,45 @@ print("Min pixel value (after normalization):", tf.reduce_min(image_batch).numpy
 print("Max pixel value (after normalization):", tf.reduce_max(image_batch).numpy())
 
 
+#------------------------------------------------------------------------------------
+#CNN Model
+model = tf.keras.Sequential([
+    #64 because larger data set 3x3 kernel
+    layers.Conv2D(64, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(2, 2),
+    
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(2, 2),
+    
+    layers.Conv2D(256, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.BatchNormalization(),
+    
+    #Fully con layers
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.Dropout(0.5),  # Prevents overfitting
+    #151 classes
+    layers.Dense(151, activation='softmax')
+])
 
+#Compile the model start with smaller learning rate for stability
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+#Train the model
+history = model.fit(
+    train_dataset,
+    epochs=20,
+    validation_data=val_dataset
+)
+
+#Model Accuracy
+test_loss, test_accuracy = model.evaluate(test_dataset)
+print(f"Test accuracy: {test_accuracy*100:.2f}%")
+
+#Save model
+model.save('Dexter_pokemon_model1.h5')
